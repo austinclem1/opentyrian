dbe4e58f9b380a16546cdf4fe9d10cae823e88d8
Austin Clements <austinclementsbass@gmail.com>
2021-10-09 12:48:33 -0500

    Update data file parsing and sprite blitting

--- sprite.zig
+++ sprite.zig
@@ -5,8 +5,15 @@ const c = @cImport({
     @cInclude("sprite.h");
 });
 
+const BlitMode = union(enum) {
+    generic,
+    blend,
+    darken,
+    filter: u8,
+};
+
 // does not clip on left or right edges of surface
-fn blit_sprite2_generic(surface: *c.SDL_Surface, x: c_int, y: c_int, sprite2s: c.Sprite2_array, index: c_uint, copyFunc: fn (*u8, *u8, ?u8) void, filter: ?u8) void {
+fn blit_sprite2_generic(surface: *c.SDL_Surface, x: c_int, y: c_int, sprite2s: c.Sprite2_array, sprite_index: c_uint, blit_mode: BlitMode) void {
     debug.assert(surface.format.*.BitsPerPixel == 8);
 
     const sprite_width = 12;
@@ -14,66 +21,74 @@ fn blit_sprite2_generic(surface: *c.SDL_Surface, x: c_int, y: c_int, sprite2s: c
     const pixel_buffer_size = @intCast(usize, surface.h * surface.pitch);
     var pixels = @ptrCast([*]u8, surface.pixels)[0..pixel_buffer_size];
 
-    var d_index = @intCast(isize, (y * surface.pitch) + x); // destination pixel index
+    // destination pixel index
+    var dest_i = @intCast(isize, (y * surface.pitch) + x);
+    // source sprite pixel index
+    var src_i: usize = 0;
 
-    // const Uint8 *data = sprite2s.data + SDL_SwapLE16(((Uint16 *)sprite2s.data)[index - 1]);
-    const sprite_data_offset: u16 = @ptrCast([*]align(1) u16, sprite2s.data)[index - 1];
+    const sprite_data_offset: u16 = @ptrCast([*]align(1) u16, sprite2s.data)[sprite_index - 1];
+    // const sprite_data_offset: u16 = @ptrCast([*]u16, sprite2s.data)[sprite_index - 1];
+    // const sprite_data_end: u16 = @ptrCast([*]align(1) u16, sprite2s.data)[sprite_index];
+    // const sprite_data: [*]u8 = @ptrCast([*]u8, &sprite2s.data[sprite_data_offset]);
+    const sprite_data: [*]u8 = @ptrCast([*]u8, &sprite2s.data[sprite_data_offset]);
     // const sprite2s_end = sprite2s.size - sprite_data_offset;
-    // var sprite_data: []u8 = sprite2s.data[sprite_data_offset..sprite2s_end];
-    var sprite_data: [*]u8 = @ptrCast([*]u8, &sprite2s.data[sprite_data_offset]);
-    var s_index: usize = 0; // source sprite pixel index
+    // var sprite_data: []u8 = sprite2s.data[sprite_data_offset..sprite_data_end];
 
-    while (sprite_data[s_index] != 0x0f) : (s_index += 1) {
-        d_index += (sprite_data[s_index] & 0x0f); // low nibble is how many empty pixels to "draw"
-        var pixels_to_write = @shrExact(sprite_data[s_index] & 0xf0, 4); // high nibble is how many pixels to copy to dest
+    while (sprite_data[src_i] != 0x0f) : (src_i += 1) {
+        dest_i += (sprite_data[src_i] & 0x0f); // low nibble is how many "empty" pixels to skip over in the dest buffer
+        var pixels_to_copy = @shrExact(sprite_data[src_i] & 0xf0, 4); // high nibble is how many pixels to copy to dest
 
-        if (pixels_to_write == 0) { // we're done with this row, advance by pitch
-            d_index += @intCast(isize, surface.pitch - sprite_width);
+        if (pixels_to_copy == 0) { // we're done with this row, advance by pitch
+            dest_i += @intCast(isize, surface.pitch - sprite_width);
             continue;
         }
-        while (pixels_to_write > 0) : (pixels_to_write -= 1) {
-            s_index += 1;
-            if (d_index >= pixel_buffer_size) return;
-            if (d_index >= 0) {
-                // pixels[@intCast(usize, d_index)] = sprite_data[s_index];
-                if (filter) |f| {
-                    copyFunc(&sprite_data[s_index], &pixels[@intCast(usize, d_index)], f);
-                } else {
-                    copyFunc(&sprite_data[s_index], &pixels[@intCast(usize, d_index)], null);
+        while (pixels_to_copy > 0) : (pixels_to_copy -= 1) {
+            src_i += 1;
+            // If we are past the end of the dest pixel buffer, nothing more can be drawn
+            if (dest_i >= pixel_buffer_size) return;
+            if (dest_i >= 0) {
+                // pixels[@intCast(usize, dest_i)] = sprite_data[src_i];
+                const src = &sprite_data[src_i];
+                var dest = &pixels[@intCast(usize, dest_i)];
+                switch (blit_mode) {
+                    .generic => dest.* = src.*,
+                    .blend => dest.* = (((src.* & 0x0f) + (dest.* & 0x0f)) / 2) | (src.* & 0xf0),
+                    .darken => dest.* = ((dest.* & 0x0f) / 2) + (dest.* & 0xf0),
+                    .filter => |f| dest.* = f | (src.* & 0x0f),
                 }
             }
-            d_index += 1;
+            dest_i += 1;
         }
     }
 }
 
 export fn blit_sprite2(surface: *c.SDL_Surface, x: c_int, y: c_int, sprite2s: c.Sprite2_array, index: c_uint) void {
-    blit_sprite2_generic(surface, x, y, sprite2s, index, copyPixel, null);
+    blit_sprite2_generic(surface, x, y, sprite2s, index, .generic);
 }
 export fn blit_sprite2_blend(surface: *c.SDL_Surface, x: c_int, y: c_int, sprite2s: c.Sprite2_array, index: c_uint) void {
-    blit_sprite2_generic(surface, x, y, sprite2s, index, copyPixelBlend, null);
+    blit_sprite2_generic(surface, x, y, sprite2s, index, .blend);
 }
 export fn blit_sprite2_darken(surface: *c.SDL_Surface, x: c_int, y: c_int, sprite2s: c.Sprite2_array, index: c_uint) void {
-    blit_sprite2_generic(surface, x, y, sprite2s, index, copyPixelDarken, null);
+    blit_sprite2_generic(surface, x, y, sprite2s, index, .darken);
 }
 export fn blit_sprite2_filter(surface: *c.SDL_Surface, x: c_int, y: c_int, sprite2s: c.Sprite2_array, index: c_uint, filter: u8) void {
-    blit_sprite2_generic(surface, x, y, sprite2s, index, copyPixelFilter, filter);
+    blit_sprite2_generic(surface, x, y, sprite2s, index, .{ .filter = filter });
 }
 
-fn copyPixel(src: *u8, dest: *u8, filter: ?u8) void {
-    _ = filter;
-    dest.* = src.*;
-}
-fn copyPixelBlend(src: *u8, dest: *u8, filter: ?u8) void {
-    _ = filter;
-    dest.* = (((src.* & 0x0f) + (dest.* & 0x0f)) / 2) | (src.* & 0xf0);
-}
-fn copyPixelDarken(src: *u8, dest: *u8, filter: ?u8) void {
-    _ = filter;
-    _ = src;
-    dest.* = ((dest.* & 0x0f) / 2) + (dest.* & 0xf0);
-}
-fn copyPixelFilter(src: *u8, dest: *u8, filter: ?u8) void {
-    _ = src;
-    dest.* = filter.? | (src.* & 0x0f);
-}
+// fn copyPixel(src: *u8, dest: *u8, filter: ?u8) void {
+//     _ = filter;
+//     dest.* = src.*;
+// }
+// fn copyPixelBlend(src: *u8, dest: *u8, filter: ?u8) void {
+//     _ = filter;
+//     dest.* = (((src.* & 0x0f) + (dest.* & 0x0f)) / 2) | (src.* & 0xf0);
+// }
+// fn copyPixelDarken(src: *u8, dest: *u8, filter: ?u8) void {
+//     _ = filter;
+//     _ = src;
+//     dest.* = ((dest.* & 0x0f) / 2) + (dest.* & 0xf0);
+// }
+// fn copyPixelFilter(src: *u8, dest: *u8, filter: ?u8) void {
+//     _ = src;
+//     dest.* = filter.? | (src.* & 0x0f);
+// }
