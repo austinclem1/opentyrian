2764981faae9c1ee7b8ee88b8c57dff8b383a36c
Austin Clements <austinclementsbass@gmail.com>
2021-10-09 12:34:52 -0500

    Clean up zig build file

--- build.zig
+++ build.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
-const LibExeObjStep = std.build.LibExeObjStep;
-const CrossTarget = std.zig.CrossTarget;
+
+const fmt = std.fmt;
+
+const Macro = struct {
+    name: []const u8,
+    value: ?[]const u8,
+};
 
 pub fn build(b: *std.build.Builder) !void {
     // Standard target options allows the person running `zig build` to choose
@@ -13,60 +18,68 @@ pub fn build(b: *std.build.Builder) !void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const objs = [_]*LibExeObjStep{ b.addObject("sprite", "src/zig/sprite.zig"), b.addObject("file", "src/zig/file.zig") };
-    for (objs) |obj| {
-        obj.addIncludeDir("src");
-        addDepPaths(obj, target);
+    var c_flags = std.ArrayList([]const u8).init(b.allocator);
+    defer c_flags.deinit();
+
+    try c_flags.append("-std=c99");
+    // try c_flags.append("-fno-sanitize-trap=undefined");
+    if (mode == .Debug) try c_flags.append("-O0");
+
+    // Macros start
+    var macros = std.ArrayList(Macro).init(b.allocator);
+    defer macros.deinit();
+
+    {
+        const tyrian_dir = b.option([]const u8, "tyrian-dir", "Path to Tyrian Installation") orelse switch (target.getOsTag()) {
+            .windows => "\"C:/TYRIAN\"",
+            .linux => "\"/usr/local/share/games/tyrian\"",
+            else => @panic("Unsupported OS"),
+        };
+        try macros.append(.{ .name = "TYRIAN_DIR", .value = tyrian_dir });
+
+        if (b.option(bool, "net-capable", "Include online multiplayer capabilities")) |_| {
+            try macros.append(.{ .name = "WITH_NETWORK", .value = null });
+        }
+
+        switch (target.getOsTag()) {
+            .windows => {
+                try macros.append(.{ .name = "TARGET_WIN32", .value = null });
+            },
+            .linux => {
+                try macros.append(.{ .name = "TARGET_UNIX", .value = null });
+            },
+            else => {},
+        }
     }
+    // Macros end
 
-    const exe = b.addExecutable("opentyrian", null);
-    addDepPaths(exe, target);
-
-    switch (target.cpu_arch orelse std.builtin.cpu.arch) {
-        .x86_64 => {
-            switch (target.os_tag orelse std.builtin.os.tag) {
-                .windows => {
-                    exe.defineCMacro("TYRIAN_DIR", "\"C:/TYRIAN\"");
-                    exe.defineCMacro("TARGET_WIN32", "1");
-                    for (objs) |obj| {
-                        linkLibsWindowsAny(obj);
-                    }
-                    linkLibsWindowsAny(exe);
-                },
-                .linux => {
-                    exe.defineCMacro("TYRIAN_DIR", "\"/usr/local/share/games/tyrian\"");
-                    exe.defineCMacro("TARGET_UNIX", "1");
-                    for (objs) |obj| {
-                        linkLibsLinuxAny(obj);
-                    }
-                    linkLibsLinuxAny(exe);
-                },
-                else => {
-                    const tag = target.os_tag orelse std.builtin.os.tag;
-                    std.debug.print("Unsupported OS: {s}\n", .{@tagName(tag)});
-                    return;
-                },
-            }
-        },
-        else => {
-            const arch = target.cpu_arch orelse std.builtin.cpu.arch;
-            std.debug.print("Unsupported architecture: {s}\n", .{@tagName(arch)});
-            return;
-        },
-    }
-    // exe.defineCMacro("WITH_NETWORK", "1");
-
-    // exe.addCSourceFile("src/opentyr.c", &[_][]const u8{ "-std=c99", "--subsystem" });
-    exe.addCSourceFiles(&[_][]const u8{ "src/animlib.c", "src/arg_parse.c", "src/backgrnd.c", "src/config.c", "src/config_file.c", "src/destruct.c", "src/editship.c", "src/episodes.c", "src/file.c", "src/font.c", "src/fonthand.c", "src/game_menu.c", "src/helptext.c", "src/joystick.c", "src/jukebox.c", "src/keyboard.c", "src/lds_play.c", "src/loudness.c", "src/lvllib.c", "src/lvlmast.c", "src/mainint.c", "src/menus.c", "src/mouse.c", "src/mtrand.c", "src/musmast.c", "src/network.c", "src/nortsong.c", "src/nortvars.c", "src/opentyr.c", "src/opl.c", "src/palette.c", "src/params.c", "src/pcxload.c", "src/pcxmast.c", "src/picload.c", "src/player.c", "src/scroller.c", "src/setup.c", "src/shots.c", "src/sizebuf.c", "src/sndmast.c", "src/sprite.c", "src/starlib.c", "src/std_support.c", "src/tyrian2.c", "src/varz.c", "src/vga256d.c", "src/vga_palette.c", "src/video.c", "src/video_scale.c", "src/video_scale_hqNx.c", "src/xmas.c" }, &[_][]const u8{"-std=c99"});
-
-    for (objs) |obj| {
-        obj.setTarget(target);
-        obj.setBuildMode(mode);
-        exe.addObject(obj);
-    }
-
+    const exe = b.addExecutable("opentyrian", "src/opentyr.c");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+    exe.setOutputDir(try fmt.allocPrint(b.allocator, "zig-out/{s}-{s}", .{ try target.linuxTriple(b.allocator), @tagName(mode) }));
+    exe.addIncludeDir("include");
+    linkLibs(exe);
+    exe.subsystem = b.option(std.Target.SubSystem, "subsystem", "Target subsystem (Console or Windows)");
+
+    exe.addCSourceFiles(&[_][]const u8{ "src/animlib.c", "src/arg_parse.c", "src/backgrnd.c", "src/config.c", "src/config_file.c", "src/destruct.c", "src/editship.c", "src/episodes.c", "src/file.c", "src/font.c", "src/fonthand.c", "src/game_menu.c", "src/helptext.c", "src/joystick.c", "src/jukebox.c", "src/keyboard.c", "src/lds_play.c", "src/loudness.c", "src/lvllib.c", "src/lvlmast.c", "src/mainint.c", "src/menus.c", "src/mouse.c", "src/mtrand.c", "src/musmast.c", "src/network.c", "src/nortsong.c", "src/nortvars.c", "src/opl.c", "src/palette.c", "src/params.c", "src/pcxload.c", "src/pcxmast.c", "src/picload.c", "src/player.c", "src/scroller.c", "src/setup.c", "src/shots.c", "src/sizebuf.c", "src/sndmast.c", "src/sprite.c", "src/starlib.c", "src/std_support.c", "src/tyrian2.c", "src/varz.c", "src/vga256d.c", "src/vga_palette.c", "src/video.c", "src/video_scale.c", "src/video_scale_hqNx.c", "src/xmas.c" }, c_flags.items);
+
+    const zig_objs = [_]*std.build.LibExeObjStep{ b.addObject("sprite", "src/zig/sprite.zig"), b.addObject("episodes", "src/zig/episodes.zig") };
+    for (zig_objs) |obj| {
+        exe.addObject(obj);
+        obj.setTarget(target);
+        obj.setBuildMode(mode);
+        obj.addIncludeDir("src");
+        obj.addIncludeDir("include");
+        obj.linkSystemLibrary("c");
+    }
+
+    // Define macros
+    for (macros.items) |macro| {
+        exe.defineCMacro(macro.name, macro.value);
+        for (zig_objs) |obj| {
+            obj.defineCMacro(macro.name, macro.value);
+        }
+    }
 
     exe.install();
 
@@ -80,115 +93,50 @@ pub fn build(b: *std.build.Builder) !void {
     run_step.dependOn(&run_cmd.step);
 }
 
-fn linkLibsWindowsAny(artifact: *LibExeObjStep) void {
-    artifact.linkSystemLibrary("c");
-    artifact.linkSystemLibrary("SDL2");
-    artifact.linkSystemLibrary("SDL2_net");
-    artifact.linkSystemLibrary("SDL2main");
-    artifact.linkSystemLibrary("kernel32");
-    artifact.linkSystemLibrary("user32");
-    artifact.linkSystemLibrary("gdi32");
-    artifact.linkSystemLibrary("winspool");
-    artifact.linkSystemLibrary("comdlg32");
-    artifact.linkSystemLibrary("advapi32");
-    artifact.linkSystemLibrary("shell32");
-    artifact.linkSystemLibrary("ole32");
-    artifact.linkSystemLibrary("oleaut32");
-    // exe.linkSystemLibrary("uuid");
-    // exe.linkSystemLibrary("odbc32");
-    // exe.linkSystemLibrary("odbccp32");
-}
+// Other windows libs (may be necessary for msvc abi)
+// artifact.linkSystemLibrary("kernel32");
+// artifact.linkSystemLibrary("user32");
+// artifact.linkSystemLibrary("gdi32");
+// artifact.linkSystemLibrary("winspool");
+// artifact.linkSystemLibrary("comdlg32");
+// artifact.linkSystemLibrary("advapi32");
+// artifact.linkSystemLibrary("shell32");
+// artifact.linkSystemLibrary("ole32");
+// artifact.linkSystemLibrary("oleaut32");
+// exe.linkSystemLibrary("uuid");
+// exe.linkSystemLibrary("odbc32");
+// exe.linkSystemLibrary("odbccp32");
 
-fn linkLibsLinuxAny(artifact: *LibExeObjStep) void {
-    artifact.linkSystemLibrary("c");
-    artifact.linkSystemLibrary("SDL2");
-    artifact.linkSystemLibrary("SDL2_net");
-    artifact.linkSystemLibrary("SDL2main");
-}
+fn linkLibs(artifact: *std.build.LibExeObjStep) void {
+    const target = artifact.target;
 
-fn addDepPaths(artifact: *LibExeObjStep, target: CrossTarget) void {
-    if (std.builtin.os.tag == .windows) {
-        artifact.addVcpkgPaths(.static) catch {};
-    }
-    switch (target.cpu_arch orelse std.builtin.cpu.arch) {
-        .x86_64 => {
-            switch (target.os_tag orelse std.builtin.os.tag) {
-                .windows => {
-                    switch (target.abi orelse std.builtin.abi) {
-                        .msvc => {
-                            addIncludesx86WindowsMsvc(artifact);
-                            addLibPathsx86WindowsMsvc(artifact);
-                        },
-                        .gnu => {
-                            // addIncludesx86WindowsGnu(artifact);
-                            // addLibPathsx86WindowsGnu(artifact);
-                        },
-                        else => {
-                            const abi = target.abi orelse std.builtin.abi;
-                            std.debug.print("Unsupported ABI: {s}\n", .{@tagName(abi)});
-                            return;
-                        },
-                    }
+    switch (target.getOsTag()) {
+        .windows => {
+            switch (target.getAbi()) {
+                .msvc => {
+                    artifact.addLibPath("../SDL2/lib/x64");
+                    artifact.addLibPath("../SDL2_net/lib/x64");
                 },
-                .linux => {
-                    switch (target.abi orelse std.builtin.abi) {
-                        .gnu => {
-                            addIncludesx86LinuxGnu(artifact);
-                            addLibPathsx86LinuxGnu(artifact);
-                        },
-                        else => {
-                            const abi = target.abi orelse std.builtin.abi;
-                            std.debug.print("Unsupported ABI: {s}\n", .{@tagName(abi)});
-                            return;
-                        },
-                    }
+                .gnu => {
+                    artifact.addLibPath("C:/Users/Austin/Documents/Programming/SDL2-2.0.14-mingw/x86_64-w64-mingw32/lib");
+                    artifact.addLibPath("C:/Users/Austin/Documents/Programming/SDL2_net-2.0.1/x86_64-w64-mingw32/lib");
                 },
-                else => {
-                    const tag = target.os_tag orelse std.builtin.os.tag;
-                    std.debug.print("Unsupported OS: {s}\n", .{@tagName(tag)});
-                    return;
-                },
+                else => @panic("Unsupported abi for windows"),
             }
         },
-        else => {
-            const arch = target.cpu_arch orelse std.builtin.cpu.arch;
-            std.debug.print("Unsupported architecture: {s}\n", .{@tagName(arch)});
-            return;
+        .linux => {
+            switch (target.getAbi()) {
+                .gnu => {
+                    artifact.addLibPath("C:/Users/Austin/Documents/Programming/linux_dev_libs/usr/lib/x86_64-linux-gnu");
+                },
+                else => @panic("Unsupported abi for linux"),
+            }
         },
+        else => @panic("Unsupported OS"),
     }
-}
 
-fn addIncludesx86WindowsMsvc(artifact: *LibExeObjStep) void {
-    artifact.addIncludeDir("../SDL2/include");
-    artifact.addIncludeDir("../SDL2_net/include");
-    // artifact.addIncludeDir("C:/Program Files (x86)/Microsoft Visual Studio/2019/BuildTools/VC/Tools/MSVC/14.29.30037/include");
-    // artifact.addIncludeDir("C:/Program Files (x86)/Microsoft Visual Studio/2019/BuildTools/VC/Tools/MSVC/14.29.30037/atlmfc/include");
-    // artifact.addIncludeDir("C:/Program Files (x86)/Windows Kits/10/Include/10.0.18362.0/ucrt");
-    // artifact.addIncludeDir("C:/Program Files (x86)/Windows Kits/10/include/10.0.18362.0/shared");
-    // artifact.addIncludeDir("C:/Program Files (x86)/Windows Kits/10/include/10.0.18362.0/um");
-    // artifact.addIncludeDir("C:/Program Files (x86)/Windows Kits/10/include/10.0.18362.0/winrt");
-}
-
-fn addLibPathsx86WindowsMsvc(artifact: *LibExeObjStep) void {
-    artifact.addLibPath("../SDL2/lib/x64");
-    artifact.addLibPath("../SDL2_net/lib/x64");
-}
-
-fn addIncludesx86WindowsGnu(artifact: *LibExeObjStep) void {
-    artifact.addIncludeDir("C:/Users/Austin/Documents/Programming/SDL2_net-2.0.1/x86_64-w64-mingw32/include/SDL2");
-    artifact.addIncludeDir("C:/Users/Austin/Documents/Programming/SDL2-2.0.14-mingw/x86_64-w64-mingw32/include/SDL2");
-}
-
-fn addLibPathsx86WindowsGnu(artifact: *LibExeObjStep) void {
-    artifact.addLibPath("C:/Users/Austin/Documents/Programming/SDL2-2.0.14-mingw/x86_64-w64-mingw32/lib");
-    artifact.addLibPath("C:/Users/Austin/Documents/Programming/SDL2_net-2.0.1/x86_64-w64-mingw32/lib");
-}
-
-fn addIncludesx86LinuxGnu(artifact: *LibExeObjStep) void {
-    artifact.addSystemIncludeDir("C:/Users/Austin/Documents/Programming/linux_dev_libs/usr/include/x86_64-linux-gnu");
-    artifact.addSystemIncludeDir("C:/Users/Austin/Documents/Programming/linux_dev_libs/usr/include/SDL2");
-}
-
-fn addLibPathsx86LinuxGnu(artifact: *LibExeObjStep) void {
-    artifact.addLibPath("C:/Users/Austin/Documents/Programming/linux_dev_libs/usr/lib/x86_64-linux-gnu");
+    artifact.linkSystemLibrary("c");
+    artifact.linkSystemLibrary("SDL2");
+    artifact.linkSystemLibrary("SDL2_net");
+    artifact.linkSystemLibrary("SDL2main");
 }
